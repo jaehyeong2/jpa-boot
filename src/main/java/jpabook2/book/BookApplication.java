b21aa9e6f2dd90032950f31d8c0b5590fbd11d9f
jae <wogud1514@naver.com>
2022-03-01 15:38:44 +0900

    환경설정

---
+++ src/main/java/jpabook2/book/BookApplication.java
@@ -0,0 +1,13 @@
+package jpabook2.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookApplication.class, args);
+    }
+
+}
